29582af280baca2801d301176d369cd4218f4e46
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2022-09-07 16:06:34 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ApiScreen from './src/screens/Api Screen'
 
 export default function App() {
   return (
-    <ApiScreen/>
+
   );
 }
 
