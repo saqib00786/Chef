4fec977fef2653e6e4d0d0a7ba75db4289c5dda8
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2022-09-07 16:39:09 +0500

    initial commit

---
+++ src/navigation/index.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <View>
+     
+    
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+
+export default Navigation
