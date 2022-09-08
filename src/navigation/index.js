af33fb739a3695d86cd58237fc1027d1d07d9467
Haris <harisnaveed1997@gmail.com>
2022-09-08 22:14:33 +0500

    first

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,33 +1,29 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { NavigationContainer } from "@react-navigation/native"
-import Splash from '../screens/Splash'
-import Main from '../screens/Main'
-import Detail from '../screens/Detail'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Splash from "../screens/Splash";
+import Main from "../screens/Main";
+import Detail from "../screens/Detail";
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name='Splash'
+          name="Splash"
           component={Splash}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Main'
+          name="Main"
           component={Main}
           options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name='Detail'
-          component={Detail}
-        />
+        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
--- src/screens/Detail/index.js
+++ src/screens/Detail/index.js
@@ -1,16 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Detail = () => {
-    return (
-        <View>
-            <Text>index</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>This is detail</Text>
+    </View>
+  );
+};
 
+const styles = StyleSheet.create({});
 
-
-const styles = StyleSheet.create({})
-
-export default Detail
+export default Detail;
